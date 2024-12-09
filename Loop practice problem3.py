4551e5478766ebc2e8c5b5dc35c0b86165330089
MD. Akil Mahmud <mdakilmahmud.ak@gmail.com>
2024-12-09 23:46:02 +0600

    Loop practice problem3.py

--- Loop practice problem3.py
+++ Loop practice problem3.py
@@ -1,5 +1,6 @@
-n=int(input('Enter a number to start the loop: '))
-n1=1
-for i in range(n):
-      print(n1,end="," if i!=n-1 else" ")
-      n1=1-n1
+num = int(input('N = '))
+for i in range(num):
+      if i%2==0:
+            print(1,end=",")
+      else:
+            print(0,end=",")
